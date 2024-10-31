4e5aa38b66ed9f586b916b4ef2b8e3612b7ffaf3
octaviaaputri <159629645+octaviaaputri@users.noreply.github.com>
2024-10-31 12:34:21 +0700

    update views

--- gitthesis/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from gitthesis.models import *
-
-# Register your models here.
-
-admin.site.register(Project)
--- gitthesis/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from django.contrib.auth.models import User
-
-class ProfileImageForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ['profile_picture']
